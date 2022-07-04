419b80866b44717cdc453e67136fec6435fb1c45
Hieu4120 <1851061734@e.tlu.edu.vn>
2022-07-04 21:07:19 +0700

    Hi

---
+++ MvcMovie/MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movies>? Movies { get; set; }// list phim
+        public SelectList? Genres { get; set; }// lis the loai
+        public string? MovieGenre { get; set; }//the loai dc selected
+        public string? SearchString { get; set; }// text ma nguoi dung nhap
+    }
+}
+
---
+++ MvcMovie/MvcMovie/Models/SeedData.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                if (context.Movies.Any())
+                {
+                    return;
+                }
+                context.Movies.AddRange(
+                    new Movies
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Rating = "R",
+                        Price = 7.99M
+                    },
+
+                    new Movies
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Rating = "S",
+                        Price = 8.99M
+                    },
+
+                    new Movies
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Rating = "G",
+                        Price = 9.99M
+                    },
+
+                    new Movies
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Rating = "K",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
